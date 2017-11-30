0f4251dbc0d5a00e5e1008569c452ce275380026
Topher Dykes <cdykes@glados>
2017-11-30 12:26:30 +0000

    Adding script to perform automatic setup of files

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt()
+{
+	rsync --exclude-from "exclude_me.txt" -avh --no-perms . ~;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This will overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+
+unset doIt;
